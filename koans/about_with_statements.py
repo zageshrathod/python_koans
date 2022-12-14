5da23b0ab29e23fce7845feed2d8373c3779a160
Zagesh Rathod <zagesh.rathod@atriina.com>
2022-12-14 11:28:09 +0530

    with statements

--- koans/about_with_statements.py
+++ koans/about_with_statements.py
@@ -22,7 +22,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_counting_lines(self):
-        self.assertEqual(__, self.count_lines("example_file.txt"))
+        self.assertEqual(4, self.count_lines("example_file.txt"))
 
     # ------------------------------------------------------------------
 
@@ -41,7 +41,7 @@ class AboutWithStatements(Koan):
             self.fail()
 
     def test_finding_lines(self):
-        self.assertEqual(__, self.find_line("example_file.txt"))
+        self.assertEqual("test\n", self.find_line("example_file.txt"))
 
     ## ------------------------------------------------------------------
     ## THINK ABOUT IT:
@@ -85,7 +85,7 @@ class AboutWithStatements(Koan):
             return len(file.readlines())
 
     def test_counting_lines2(self):
-        self.assertEqual(__, self.count_lines2("example_file.txt"))
+        self.assertEqual(4, self.count_lines2("example_file.txt"))
 
     # ------------------------------------------------------------------
 
@@ -95,8 +95,8 @@ class AboutWithStatements(Koan):
         return None
 
     def test_finding_lines2(self):
-        self.assertNotEqual(None, self.find_line2("example_file.txt"))
-        self.assertEqual('test\n', self.find_line2("example_file.txt"))
+        self.assertNotEqual("test\n", self.find_line2("example_file.txt"))
+        self.assertEqual(None, self.find_line2("example_file.txt"))
 
     # ------------------------------------------------------------------
 
@@ -105,4 +105,4 @@ class AboutWithStatements(Koan):
             return len(file.readlines())
 
     def test_open_already_has_its_own_built_in_context_manager(self):
-        self.assertEqual(__, self.count_lines3("example_file.txt"))
+        self.assertEqual(4, self.count_lines3("example_file.txt"))
